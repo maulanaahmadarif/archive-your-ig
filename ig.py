3ee2e7b7ef26e17a31350d80fef010fa4cc097ae
Arif Maulana <arif.maulana@ovo.id>
2019-09-11 17:37:58 +0700

    Initial commit

---
+++ ig.py
@@ -0,0 +1,78 @@
+import urllib.request
+import json
+
+def getIGJson(payload):
+  startIndex = response.find("{\"config\":")
+  resStartIndex = response[startIndex:]
+  lastIndex = resStartIndex.find(";</script>")
+  resLastIndex = resStartIndex[:lastIndex]
+
+  jsonFormat = json.loads(resLastIndex)
+  return jsonFormat
+
+def getExcerptTitle(title):
+  title = title.replace('/', '')
+  title = title.replace('\n', '')
+  if len(title) >= 15:
+    return title[:15]
+  return title
+
+link = "https://www.instagram.com/pevpearce/"
+f = urllib.request.urlopen(link)
+myfile = f.read()
+response = myfile.decode('utf-8')
+
+jsonFormat = getIGJson(response)
+
+timelines = jsonFormat["entry_data"]["ProfilePage"][0]["graphql"]["user"]["edge_owner_to_timeline_media"]["edges"]
+
+count = 0
+
+for node in timelines:
+  if node["node"]["__typename"] == 'GraphVideo':
+    linkId = node["node"]["shortcode"]
+    videoLink = "https://www.instagram.com/p/" + linkId
+    f = urllib.request.urlopen(videoLink)
+    myfile = f.read()
+    response = myfile.decode('utf-8')
+    jsonVideo = getIGJson(response)
+    videoURL = jsonVideo["entry_data"]["PostPage"][0]["graphql"]["shortcode_media"]["video_url"]
+    postId = jsonVideo["entry_data"]["PostPage"][0]["graphql"]["shortcode_media"]["id"]
+    caption = ""
+    if len(jsonVideo["entry_data"]["PostPage"][0]["graphql"]["shortcode_media"]["edge_media_to_caption"]["edges"]) != 0:
+      caption = jsonVideo["entry_data"]["PostPage"][0]["graphql"]["shortcode_media"]["edge_media_to_caption"]["edges"][0]["node"]["text"].encode('utf-8').decode()
+    urllib.request.urlretrieve(videoURL, getExcerptTitle(caption) + "-" + postId + ".mp4")
+    print(getExcerptTitle(caption) + "-" + postId + "=> downloaded 🎉")
+
+  elif node["node"]["__typename"] == 'GraphSidecar':
+    linkId = node["node"]["shortcode"]
+    slidesLink = "https://www.instagram.com/p/" + linkId
+    f = urllib.request.urlopen(slidesLink)
+    myfile = f.read()
+    response = myfile.decode('utf-8')
+    jsonSlide = getIGJson(response)
+
+    
+    slidesId = jsonSlide["entry_data"]["PostPage"][0]["graphql"]["shortcode_media"]["id"]
+    slides = jsonSlide["entry_data"]["PostPage"][0]["graphql"]["shortcode_media"]["edge_sidecar_to_children"]["edges"]
+    slideCaption = ""
+    if len(jsonSlide["entry_data"]["PostPage"][0]["graphql"]["shortcode_media"]["edge_media_to_caption"]["edges"]) != 0:
+      slideCaption = jsonSlide["entry_data"]["PostPage"][0]["graphql"]["shortcode_media"]["edge_media_to_caption"]["edges"][0]["node"]["text"]
+    
+    count = 1
+    for slideNode in slides:
+      slideURL = slideNode["node"]["display_resources"][2]["src"].encode('utf-8').decode()
+      urllib.request.urlretrieve(slideURL, getExcerptTitle(slideCaption) + "-" + str(count) + slidesId + ".jpg")
+      count += 1
+      print(getExcerptTitle(slideCaption) + "-" + slidesId + "=> downloaded 🎉")
+    
+  else:
+    postId = node["node"]["id"]
+    filename = ""
+    if len(node["node"]["edge_media_to_caption"]["edges"]) != 0:
+      filename = node["node"]["edge_media_to_caption"]["edges"][0]["node"]["text"].encode('utf-8').decode()
+    
+    imageURL = node["node"]["thumbnail_resources"][4]["src"].encode('utf-8').decode()
+    urllib.request.urlretrieve(imageURL, getExcerptTitle(filename) + "-" + postId + ".jpg")
+    print(getExcerptTitle(filename) + "-" + postId + "=> downloaded 🎉")
+
